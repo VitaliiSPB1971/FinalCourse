a5ede6187bb5ff3a414a84fd6d0be96e5ea9a8fe
Vitalii <telexp@yandex.ru>
2022-11-19 12:14:27 +0300

    init: Array creation method

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Метод создания массива из менее, чем трёх символов
+string[] CreateNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
+}
